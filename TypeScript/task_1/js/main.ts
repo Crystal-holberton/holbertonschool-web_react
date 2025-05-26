27b8a39cda5aab1c20e4afeb8abae6dba1a1723e
Crystal-holberton <9188@holbertonstudents.com>
2025-05-26 14:26:52 +1000

    Class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -35,12 +35,38 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+};
+
+interface StudentInterface{
+  workOnHomework(): string;
+  displayName(): string;
+};
+
+class StudentClass implements StudentInterface {
+  constructor(private firstName: string, private lastName: string) {}
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
 console.log('Teacher:', teacher3);
 
 console.log('Director:', director1);
 
 console.log(printTeacher("John", "Doe"));
 
-const p = document.createElement('p');
-p.textContent = printTeacher("John", "Doe");
-document.body.appendChild(p);
+const student = new StudentClass("Bob", "Johnson");
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
+const Name = document.createElement("p");
+Name.textContent = "Name: " + student.displayName();
+const homework = document.createElement("p");
+homework.textContent = student.workOnHomework();
+document.body.appendChild(Name);
+document.body.appendChild(homework);
