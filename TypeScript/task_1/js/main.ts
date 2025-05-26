da5f5edeafaf4dbc073fb40e256b9ee2eddfc0f9
Crystal-holberton <9188@holbertonstudents.com>
2025-05-26 13:28:57 +1000

    Teacher interface

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log('Teacher:', teacher3);
+
+const el = document.createElement('pre');
+el.textContent = JSON.stringify(teacher3, null, 2);
+document.body.appendChild(el);
